03fe6c53b7c8f187d03ce6bdbe3b8ad4ac287d53
Matt Cornall <33593869+matt-cornall@users.noreply.github.com>
2022-11-10 22:47:14 +0000

    Corrected errors with NSG association

--- Terraform Templates/VM-Linux-NSG-NoDB/main.tf
+++ Terraform Templates/VM-Linux-NSG-NoDB/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_virtual_network" "vnet_1" {
 resource "azurerm_subnet" "subnet_1" {
   name                 = "Subnet-1"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet1.name
+  virtual_network_name = azurerm_virtual_network.vnet_1.name
   address_prefixes     = ["10.10.10.0/24"]
 }
 
@@ -65,7 +65,7 @@ resource "azurerm_network_interface" "vm1_nic" {
 }
 
 # Connect the security group to the network interface
-resource "azurerm_network_interface_security_group_association" "nsg_subnet_association" {
+resource "azurerm_subnet_network_security_group_association" "nsg_subnet_association" {
   subnet_id      = azurerm_subnet.subnet_1.id
   network_security_group_id = azurerm_network_security_group.vm1_nsg.id
 }
