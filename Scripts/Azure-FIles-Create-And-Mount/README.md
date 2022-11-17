7be44b5a20963c74ee1f93585d28b1e4c5f55ead
Matt Cornall <33593869+matt-cornall@users.noreply.github.com>
2022-11-17 16:00:24 +0000

    Added create and mount share script

---
+++ Scripts/Azure-FIles-Create-And-Mount/README.md
@@ -0,0 +1,27 @@
+# Azure Files - Create and Mount script
+This script automatically creates and mounts Azure Files shares in Linux VMs, leveraging Managed Identities to authorise the API calls
+
+# Requirements
+- cifs-utils and jq packages are installed on the Linux VM
+    dnf/apt-get install cifs-utils - package required to mount SMB shared on Linux
+    dnf/apt-get install jq - package required to parse JSON data to retrieve Azure AD token
+- Managed Identitiy enabled for the VM running this script
+    System-Assigned Managed Identitify is enabled
+    The folloiwng roles are assigned
+        Storage Account Contributor
+        Files SMB Elavated Contributor
+        Resource Group Reader
+- You have prepared the script by editing the variables 
+    sub-id - the long alphanumeric string of your subscription
+    rg - the name of the resource group that this VM and Azure Files instance reside in
+    storacc - the name of the Storage Account to be used to create Files shares
+    sak - the PRIMARY Access Key of the above Storage Account
+    settings - the settings for the Files share
+        Tier: Transaction Optimized, Hot or Cool
+        Quota: 1-5120 (GBs - standard shares have a 5TB limit)
+- You have made the script executable
+    sudo chmod -x create-and-mount-afshare.sh
+
+# Running the script
+Simply run the below command, replacing "example" with the desired name of the share to be created and mounted
+    bash ./create-and-mount-afshare.sh example
---
+++ Scripts/Azure-FIles-Create-And-Mount/create-and-mount-afshare.sh
@@ -0,0 +1,38 @@
+# Provide a name for the new Share
+share=$1
+
+# Set your variables here
+subid=subscription-id
+storacc=storage-account-name
+rg=rg-resource-group-name
+sak=storage-access-key-primary
+settings="{'properties': {'accessTier': 'Cool', 'shareQuota': '1024'}}"
+
+# Collecting token from Azure AD via VM Managed Identitify
+token=$(curl -X GET 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/' -H Metadata:true | jq --raw-output .access_token)
+
+# Calling Azure Storage Service to create share
+curl -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer $token" -X PUT -d "$settings" "https://management.azure.com/subscriptions/$subid/resourceGroups/$rg/providers/Microsoft.Storage/storageAccounts/$storacc/fileServices/default/shares/$share?api-version=2022-05-01"
+
+# Pause while storage is provisioned
+echo
+echo
+echo "Waiting to mount while File share is created...(10 seconds)"
+sleep 10
+echo
+
+# Create directory and mount storage
+sudo mkdir /mnt/$share
+if [ ! -d "/etc/smbcredentials" ]; then
+sudo mkdir /etc/smbcredentials
+fi
+if [ ! -f "/etc/smbcredentials/$storacc.cred" ]; then
+    sudo bash -c "echo "username=$storacc" >> /etc/smbcredentials/$storacc.cred"
+    sudo bash -c "echo "password=$sak" >> /etc/smbcredentials/$storacc.cred"
+fi
+sudo chmod 600 /etc/smbcredentials/$storacc.cred
+sudo bash -c "echo "//$storacc.file.core.windows.net/$share /mnt/$share cifs nofail,credentials=/etc/smbcredentials/$storacc.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30" >> /etc/fstab"
+sudo mount -t cifs //$storacc.file.core.windows.net/$share /mnt/$share -o credentials=/etc/smbcredentials/$storacc.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30 
+echo
+echo "Complete."
+echo
